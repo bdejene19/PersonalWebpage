929617d1c51b47d7d08956cacdee348840d5ca8b
Bemnet Dejene <bdejene19@gmail.com>
2020-12-11 12:03:03 -0500

    Updates

---
+++ precache-manifest.23d818602e37bd5f5613a6d10be7f35a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531cfc96c9da86528d9e90bd5059a811",
+    "url": "/PersonalWebpage/index.html"
+  },
+  {
+    "revision": "4b02eebe3e83fecd8f39",
+    "url": "/PersonalWebpage/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "79363c096f9cd91d0cfa",
+    "url": "/PersonalWebpage/static/js/2.36a49d36.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PersonalWebpage/static/js/2.36a49d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b02eebe3e83fecd8f39",
+    "url": "/PersonalWebpage/static/js/main.3e021c2f.chunk.js"
+  },
+  {
+    "revision": "a5afd8fd8d8adacf5fb2",
+    "url": "/PersonalWebpage/static/js/runtime-main.1ad03848.js"
+  }
+]);
